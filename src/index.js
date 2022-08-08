f94584805ca77e367b929977c4fa1fd0cdf538f2
Philipp Dudarenko <iphilimosha@gmail.com>
2022-08-08 19:49:29 +0300

    init: revers int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n),
+        strNew = '',
+        rez = 0;
+
+    str = str.replace(/\-/g, '');
+
+    for(let i = str.length-1; i >= 0; i--) {
+        strNew = strNew.concat(str[i]);
+    } 
+    return rez = Number(strNew);
 }
